74467d5dbc1f944a2146383847a0409ab3f87e6c
Arthur Camurça <arthur.04.pb@gmaiil.com>
2023-05-18 21:22:49 -0300

    [ADD] # - Enviando e-mails ao realizar cadastro de usuário.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,24 +5,44 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\RawMessage;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
+    private MailerInterface $mailer;
+
+    public function __construct(
+        MailerInterface $mailer
+    )
+    {
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager
+    ): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -32,8 +52,15 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
 
+            $email = (new TemplatedEmail())
+                ->to($user->getEmail())
+                ->subject('Cadastro realizado com sucesso!')
+                ->text('Seu cadastro em nossa plataforma foi realizado com sucesso.')
+                ->htmlTemplate('emails/welcome.html.twig')
+                ->context(compact('user'));
+
+            $this->mailer->send($email);
             return $this->redirectToRoute('series');
         }
 
